d93e3f7c2eeb0af954c5ee97a52caf04f3455481
Anand Thyagaraju <anand.thyagaraju@2876613.no-reply.drupal.org>
2016-12-29 18:57:23 +0530

    Initial commit:added all basic functionalities

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # country-tz-currency
+
+## Install
+```
+npm install country-tz-currency --save
+```
+## Usage
+
+```
+var ctzc 	=	require('country-tz-currency');
+
+ctzc.getAllCountries() //Returns list of country objects
+
+ctzc.getAllTimezones() //Returns list of timezone objects
+
+ctzc.getAllCurrencies() //Returns list of currency objects
+```
---
+++ src/index.js
@@ -0,0 +1,64 @@
+var data 	=	require( '../data/data.json' );
+
+
+var countryTzCurrency 	=	function()
+{
+	this.getAllCountries 				= function()
+	{
+		return data.countries;
+	}
+
+	this.getAllTimezones 				=	function()
+	{
+		return data.timezones;
+	}
+
+	this.getAllCurrencies 				=	function()
+	{
+		return data.currencies;
+	}
+
+	this.getCountryByCode 				= 	function( code )
+	{
+		return this.getAllCountries()[code];
+	}
+
+	this.getCurrencyByCode 				=	function( code )
+	{
+		return this.getAllCurrencies()[code];
+	}
+
+	this.getCurrencyByCountryCode 		= 	function( code )
+	{
+		var country 		=	this.getAllCountries()[code];
+		return country ? this.getCurrencyByCode( country.currencyCode ) : undefined;
+	}
+
+	this.getTzById						=	function( timezoneId )
+	{
+		return this.getAllTimezones()[timezoneId];
+	}
+
+	this.getTzIdsByCountryCode 			=	function( code )
+	{
+		var country 		=	this.getAllCountries()[code];
+		return country ? country.timeZone : undefined;
+	}
+
+	this.getTzByCountryCode 			=	function( code )
+	{
+		var tzIds 			=	this.getTzIdsByCountryCode( code );
+		if( tzIds )
+		{
+			var tzs 		=	[];
+			var len 		=	tzIds.length;
+			for( var i=0; i<len; i++ )
+			{
+				tzs.push( this.getTzById( tzIds[i] ) );
+			}
+			return tzs;
+		}
+	}
+}
+
+module.exports = new countryTzCurrency();
