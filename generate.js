8c5fd12ab52162409fe1e4ffced810158f4491c2
Ronald Kasendwa <rkasendwa@outlook.com>
2024-02-13 10:40:29 +0300

    Remove generate.js file

--- generate.js
+++
@@ -1,9 +0,0 @@
-var ctzc = require('./src/index.js');
-
-console.log('Default locale: ' + ctzc.getLocale());
-
-ctzc.setLocale('fr');
-
-console.log('Selected locale: ' + ctzc.getLocale());
-
-console.log('\n\n' + JSON.stringify(ctzc.getAllCurrencies(), null, 2) + '\n\n');
